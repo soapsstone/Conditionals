fde6156beae6e14a9e6bc17994cefd23c7bd0d14
Sophia Spaulding <soapsstone@gmail.com>
2020-12-10 18:14:22 -0500

    Conditionals

---
+++ javascript.js
@@ -0,0 +1,130 @@
+// condtionals - if else statements
+
+//if(2 + 2 === 4){
+//    console.log("we can math!!!")
+//}
+
+
+//let myName = "Sloan";
+
+
+// if(myName === "Sophia"){
+    // console.log("where's the pizza?")
+// }
+// else{
+    // console.log("you should be Sophia")
+// }
+
+
+// let isHungry = true;
+
+// if(isHungry){
+    // console.log("you should go eat!!!")
+// }
+
+// let a = 2;
+// let b = 3;
+// let c = 4;
+
+//&& means both conditions have to be correct
+
+// if(a + b === 5 && a + c === 6){
+    // console.log("You Rock!!!")
+// }
+// else{
+    // console.log("bad math")
+// }
+
+//Lightnight Exercise 1
+
+// let likesToTravel = true;
+
+// if(likesToTravel === true){
+    // console.log("Bon voyage!")
+// }
+// else{
+    // console.log("Enjoy your couch.")
+// }
+
+//Lightning Exercise 2
+
+// Fried Pickles are the Best
+//In your JavaScript file, declare a new variable called favoriteFood and set it equal to your favorite food.
+// Write a conditional that checks to see if the favoriteFood variable is equal to "fried pickles" OR "pepperoni rolls". If so, log "Congratulations! You have excellent taste!" to the console. Otherwise, log "Clearly you have not tried fried pickles or pepperoni rolls." to the console.
+
+
+// let favoriteFood = "pizza"
+
+// if(favoriteFood === "fried pickles" || favoriteFood === "pepperoni rolls"){
+    // console.log("Congratulations, you have excellent taste!")
+// }
+// else{
+    // console.log("Clearly you have not tried fried pickles or pepperoni rolls.")
+//} // should be Clearly you have not...
+
+
+//Practie
+
+//P-1
+
+if(1 + 1 < 5){
+    console.log("Lies!")
+}
+else{
+    console.log("True Fact!")
+}
+
+if(4 * 5 > 20){
+    console.log("Lies!")
+}
+else{
+    console.log("True Fact!")
+}
+
+if(6 / 2 < 0){
+    console.log("Lies!")
+}
+else{
+    console.log("True Fact!")
+}
+
+
+//P-2
+
+let likesDogs = true
+
+if(likesDogs === true){
+    console.log("You're a dog person!")
+}
+
+
+//P-3
+
+let birthYear = 1996
+
+if(birthYear = 1995-2012){
+    console.log("You're in the Generation Z category.")
+}
+else{
+    console.log("You are too old for that generation!")
+}
+
+
+//P-4
+
+let person = {
+    name: "Andrew",
+    preferredLang: "English",
+}
+
+if(person.preferredLang = "English"){
+    console.log("Hello, Andrew!")
+}
+
+if(preferredLang = "Spanish"){
+    console.log("Hola, Andrew!")
+}
+
+if(prefereedLang = "French"){
+    console.log("Bonjour, Andrew!")
+}
